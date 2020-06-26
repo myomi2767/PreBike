0f10825dfb8ce99fa283e7447e686a5184719b68
myomi <myomi0618@gmail.com>
2020-06-26 14:35:02 +0900

    * template 적용

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "board"
+
 urlpatterns = [
-    
+    path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('password/', views.password, name="password"),
+    path('charts/', views.charts, name="charts"),
+    path('tables/', views.tables, name="tables"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'board/index.html')
+
+def login(request):
+    return render(request, 'board/login.html')
+
+def register(request):
+    return render(request, 'board/register.html')
+
+def password(request):
+    return render(request, 'board/password.html')
+
+def charts(request):
+    return render(request, 'board/charts.html')
+
+def tables(request):
+    return render(request, 'board/tables.html')
